2c02425768fc3d0e0b024da9aeb0bdd145db7e93
Mrinmayee Rane <mrinmayeerane2810@gmail.com>
2025-03-12 19:45:09 +0530

    Moved everything to their own components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import "./categories.styles.scss";
+import CategoryMenu from "./components/category-menu/category-menu.components";
 import { categories } from "./data/categories";
+
 const App = () => {
-  return (
-    <div className="categories-container">
-      {categories.map(({ id, title, imageUrl }) => (
-        <div key={id} className="category-container">
-          <div
-            className="background-image"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          />
-          <div className="category-body-container">
-            <h2>{title}</h2>
-            <p>Shop Now</p>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
+  return <CategoryMenu categories={categories} />;
 };
 
 export default App;
